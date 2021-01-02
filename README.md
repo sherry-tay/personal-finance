e6a31ca5a8246e6ce8c3e70c784332490dea3780
Sherry <sherrytay95@gmail.com>
2021-01-02 19:14:51 +0800

    Added python script to update holdings in Firestore

--- README.md
+++ README.md
@@ -20,3 +20,16 @@ To update SGX securities category info:
 ```
 python .\scripts\fetch_code.py
 ```
+
+To update stock holdings in FireStore:
+
+1. Add a JSON file in `data` folder following the format of `stocks.json` in the `templates` directory
+
+1. Run the following:
+
+```
+pip3.9 install --upgrade firebase-admin
+python3 .\scripts\update_firestore.py
+```
+
+Note: `pip3.9 install` only needs to be run if Firebase SDK has not yet been installed
---
+++ scripts/update_firestore.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import json
+import datetime
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+serviceAccountFilePath = 'internal/firebase/personal-finance-admin.json'
+input_file = 'data/stocks.json'
+stocksCollectionName = 'stocks'
+
+cred = credentials.Certificate(serviceAccountFilePath)
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+with open(input_file) as json_file:
+    data = json.load(json_file)
+    for entry in data:
+        date = entry["date"]
+        entry["date"] = datetime.datetime(int(date[0:4]), int(date[4:6]), int(date[6:8]), tzinfo=datetime.timezone(datetime.timedelta(hours=8)))
+        documentId = date + "-" + entry["code"]
+        db.collection(stocksCollectionName).document(documentId).set(entry)
