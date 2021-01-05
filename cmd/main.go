55e58508338da33abb2bd42ab9eddbcb65abbda1
Sherry Tay <sherrytay95@gmail.com>
2021-01-05 20:13:55 +0800

    Display statistics from Telegram bot

--- cmd/main.go
+++ cmd/main.go
@@ -1,38 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"math"
-
-	"personal.finance/internal/firebase"
-	"personal.finance/internal/sgx"
 	"personal.finance/internal/telegram"
 )
 
 func main() {
-	totalPortfolio, totalInvested := 0.0, 0.0
-	averagePrice := firebase.GetHoldings()
-	for key, value := range averagePrice {
-		current := sgx.GetCurrentPrice(key)
-		diff, percentage, profit := getProfit(current, value.Price, value.Volume)
-		fmt.Printf("%v: Current %v, Average %v, Difference %v (%v%%), Volume %v, Profit %v\n", key, current, value.Price, diff, percentage, value.Volume, profit)
-		totalPortfolio += current * float64(value.Volume)
-		totalInvested += value.Price * float64(value.Volume)
-	}
-	totalDiff, totalPercentage, _ := getProfit(totalPortfolio, totalInvested, 0)
-	fmt.Printf("Current portfolio: %v\n", round(totalPortfolio))
-	fmt.Printf("Total invested: %v\n", round(totalInvested))
-	fmt.Printf("Capital gains: %v (%v%%)\n", totalDiff, totalPercentage)
-
 	telegram.Initialize()
-}
-
-func getProfit(current, average float64, volume int) (float64, float64, float64) {
-	diff := (current - average)
-	return round(diff), round(diff/average*100), round(diff * float64(volume))
-}
-
-func round(val float64) float64 {
-	precisionFactor := 1000.0
-	return math.Round(val * precisionFactor)/precisionFactor
 }
--- internal/telegram/telegram.go
+++ internal/telegram/telegram.go
@@ -1,7 +1,12 @@
 package telegram
 
 import (
+	"fmt"
 	"log"
+	"math"
+
+	"personal.finance/internal/firebase"
+	"personal.finance/internal/sgx"
 
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
@@ -35,8 +40,8 @@ func Initialize() {
 		if update.Message.IsCommand() {
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 			switch update.Message.Command() {
-			case "help":
-				msg.Text = "type /sayhi or /status."
+			case "portfolio":
+				msg.Text = getStatistics()
 			case "sayhi":
 				msg.Text = "Hi :)"
 			case "status":
@@ -52,4 +57,32 @@ func Initialize() {
 			bot.Send(msg)
 		}
 	}
+}
+
+func getStatistics() string {
+	message := ""
+	totalPortfolio, totalInvested := 0.0, 0.0
+	averagePrice := firebase.GetHoldings()
+	for key, value := range averagePrice {
+		current := sgx.GetCurrentPrice(key)
+		diff, percentage, profit := getProfit(current, value.Price, value.Volume)
+		message += fmt.Sprintf("%v: Current %v, Average %v, Difference %v (%v%%), Volume %v, Profit %v\n", key, current, value.Price, diff, percentage, value.Volume, profit)
+		totalPortfolio += current * float64(value.Volume)
+		totalInvested += value.Price * float64(value.Volume)
+	}
+	totalDiff, totalPercentage, _ := getProfit(totalPortfolio, totalInvested, 0)
+	message += fmt.Sprintf("Current portfolio: %v\n", round(totalPortfolio))
+	message += fmt.Sprintf("Total invested: %v\n", round(totalInvested))
+	message += fmt.Sprintf("Capital gains: %v (%v%%)\n", totalDiff, totalPercentage)
+	return message
+}
+
+func getProfit(current, average float64, volume int) (float64, float64, float64) {
+	diff := (current - average)
+	return round(diff), round(diff/average*100), round(diff * float64(volume))
+}
+
+func round(val float64) float64 {
+	precisionFactor := 1000.0
+	return math.Round(val * precisionFactor)/precisionFactor
 }
