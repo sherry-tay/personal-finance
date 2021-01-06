acf336c04c9b9a4273137ccbf96891f879830c86
Sherry Tay <sherrytay95@gmail.com>
2021-01-06 12:02:36 +0800

    Formatted table for information displayed on bot

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	firebase.google.com/go v3.13.0+incompatible
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/olekukonko/tablewriter v0.0.4
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 	google.golang.org/api v0.36.0
 )
--- internal/telegram/telegram.go
+++ internal/telegram/telegram.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"log"
 	"math"
+	"strings"
 
 	"personal.finance/internal/firebase"
 	"personal.finance/internal/sgx"
 
 	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/olekukonko/tablewriter"
 )
 
 func Initialize() {
@@ -41,6 +43,7 @@ func Initialize() {
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 			switch update.Message.Command() {
 			case "portfolio":
+				msg.ParseMode = "Markdown"
 				msg.Text = getStatistics()
 			case "sayhi":
 				msg.Text = "Hi :)"
@@ -60,21 +63,36 @@ func Initialize() {
 }
 
 func getStatistics() string {
-	message := ""
+	var data []stockInfo
 	totalPortfolio, totalInvested := 0.0, 0.0
+
 	averagePrice := firebase.GetHoldings()
+
 	for key, value := range averagePrice {
 		current := sgx.GetCurrentPrice(key)
 		diff, percentage, profit := getProfit(current, value.Price, value.Volume)
-		message += fmt.Sprintf("%v: Current %v, Average %v, Difference %v (%v%%), Volume %v, Profit %v\n", key, current, value.Price, diff, percentage, value.Volume, profit)
+		
+		s := stockInfo {
+			Code: key,
+			CurrentPrice: current,
+			Average: value.Price,
+			Diff: diff,
+			DiffPercentage: percentage,
+			Volume: value.Volume,
+			Profit: profit,
+		}
+		data = append(data, s)
+
 		totalPortfolio += current * float64(value.Volume)
 		totalInvested += value.Price * float64(value.Volume)
 	}
+
 	totalDiff, totalPercentage, _ := getProfit(totalPortfolio, totalInvested, 0)
-	message += fmt.Sprintf("Current portfolio: %v\n", round(totalPortfolio))
-	message += fmt.Sprintf("Total invested: %v\n", round(totalInvested))
-	message += fmt.Sprintf("Capital gains: %v (%v%%)\n", totalDiff, totalPercentage)
-	return message
+	
+	return fmt.Sprintf(
+		"```\n%v```\nCurrent portfolio: %.3f\nTotal invested: %.3f\nCapital gains: %.3f (%.3f%%)", 
+		formatTable(data), totalPortfolio, totalInvested, totalDiff, totalPercentage,
+	)
 }
 
 func getProfit(current, average float64, volume int) (float64, float64, float64) {
@@ -85,4 +103,39 @@ func getProfit(current, average float64, volume int) (float64, float64, float64)
 func round(val float64) float64 {
 	precisionFactor := 1000.0
 	return math.Round(val * precisionFactor)/precisionFactor
+}
+
+func formatTable(stockInfos []stockInfo) string {
+	var formatted [][]string
+	 for _, i := range stockInfos {
+		strings := []string { 
+			i.Code, 
+			fmt.Sprintf("%.3f", i.CurrentPrice), 
+			fmt.Sprintf("%.3f", i.Average), 
+			fmt.Sprintf("%.3f", i.Diff), 
+			fmt.Sprintf("%.3f", i.DiffPercentage), 
+			fmt.Sprintf("%v", i.Volume), 
+			fmt.Sprintf("%.3f", i.Profit),
+		}
+		formatted = append(formatted, strings)
+	 }
+
+	 tableString := &strings.Builder{}
+	 table := tablewriter.NewWriter(tableString)
+	 table.SetHeader([]string { "Code", "Now", "Avg", "Diff", "%", "Vol", "Total" })
+	 table.SetBorders(tablewriter.Border {Left: true, Top: false, Right: true, Bottom: false})
+	 table.SetCenterSeparator("|")
+	 table.AppendBulk(formatted)
+	 table.Render()
+	 return tableString.String()
+}
+
+type stockInfo struct {
+	Code string
+	CurrentPrice float64
+	Average float64
+	Diff float64
+	DiffPercentage float64
+	Volume int
+	Profit float64
 }
